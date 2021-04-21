26e26595f29d64d8432f00d9b9b9a2ef5850c87a
Aleksey <aleksey.amelchenko@gmail.com>
2021-04-21 13:40:11 +0300

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import File from "./file-sharing";
 
+const previews = [{ time: `10:36` }, { time: `5:12` }, { time: `12:20` }];
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <File previews={previews} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
